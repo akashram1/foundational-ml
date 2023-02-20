5460e965a2bb9fd8dcc3ac5c2e99b40fc9f0f86b
Akash Ramachandran <akayramz@Akashs-MacBook-Pro.local>
2023-02-20 14:29:27 -0500

    finish linreg

--- linear_models/linear_reg.py
+++ linear_models/linear_reg.py
@@ -1,8 +1,10 @@
 import numpy as np
 from enum import Enum
 
+
 class Optimization(Enum):
     NORMAL, GD = 1,2
+
 
 class LinearRegression:
     def __init__(self, fit_intercept=True, seed=2023, copy=True, optimization=Optimization.NORMAL):
@@ -20,19 +22,36 @@ class LinearRegression:
         # Add the dummy x0 variable to each row to fold intercept into weights vector
         X = np.insert(X, 0, 1, axis=1)
         n, m = X.shape[0], X.shape[1]
+
         if self.optimization == Optimization.NORMAL:
+            # (XTX)^-1 (XTY)
             inverse = np.linalg.inv(np.dot(X.T, X))
             xy = np.dot(X.T, y)
             self.weights = np.dot(inverse, xy)
         else:
-            gradient = self.rng.random((m,))
+            # Needs to be subtracted from weights. So has to be (m,)
+            self.weights = self.rng.normal(loc=0, scale=0.1, size=(m,))
 
+            # Assuming 100 epochs and learning rate = 0.01
+            learning_rate = 0.1
+            for _ in range(10000):
+                y_predicted = np.dot(X, self.weights)  # (n,m) dot (m,) = (n, )
+                gradient = np.dot(X.T, (y_predicted - y)) / n  # (m,n) dot (n, ) = (m, )
+                self.weights = self.weights - learning_rate * gradient
+
+    def predict(self, X):
+        if self.copy:
+            X = np.copy(X)
+            # Add the dummy x0 variable to each row to fold intercept into weights vector
+        X = np.insert(X, 0, 1, axis=1)
+        return np.dot(X, self.weights)
 
 
 if __name__ == '__main__':
-    linreg = LinearRegression()
+    linreg = LinearRegression(optimization=Optimization.GD)
     X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
     y = np.dot(X, np.array([1, 2])) + 3
     linreg.fit(X, y)
     assert linreg.weights.all() == np.array([3,1,2]).all()
-    print(linreg.weights)
+    print(linreg.predict(np.array([[2, 3], [4,5 ]])))
+
